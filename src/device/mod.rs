8447e8611572d89a531f03d99a43451b7c6a6cd4
Debuglines <git@debuglines.com>
2020-06-15 16:26:49 +0200

    update

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -1,6 +1,1 @@
 // pub const AIRTINGS_WAVE_PLUS_UUID: &str = "b42e2a68-ade7-11e4-89d3-123b93f75cba";
-
-// E0:7D:EA:28:2F:24
-
-// const debug: &str = "224, 0, 0, 5, 202, 102, 8, 171,";
-// const debug: &str = "52, 3, 244, 171, 164, 174, 72, 0";
